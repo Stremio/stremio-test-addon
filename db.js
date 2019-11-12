0f0ffc1c5aa6f3e58fd3a13dd6e8aaa49f1b265a
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-11-12 10:09:06 +0200

    add port to the names and descriptions

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const pullAllMetas = (port) => ({
     "pt1": {
         "id": "pt1",
         "type": "movie",
-        "name": "Pen test movie 1",
+        "name": `Pen test movie 1 ${port}`,
         "poster": `http://127.0.0.1:${port}/assets/poster.jpg`,
         "background": `http://127.0.0.1:${port}/assets/background.png`,
         "logo": `http://127.0.0.1:${port}/assets/logo.png`,
-        "description": "Pen test movie 1 description",
+        "description": `Pen test movie 1 description ${port}`,
         "releaseInfo": "2019-",
         "runtime": "10:00",
         "released": "2019-11-08T07:57:48.271Z",
@@ -51,25 +51,25 @@ const pullAllMetas = (port) => ({
 const pullAllVideos = (port) => ({
     "pt1v1": {
         "id": "pt1v1",
-        "title": "Pen test movie 1 video 1",
+        "title": `Pen test movie 1 video 1 ${port}`,
         "thumbnail": `http://127.0.0.1:${port}/assets/poster.jpg`,
         "released": "2019-11-08T07:57:48.271Z",
-        "overview": "Pen test movie 1 video 1 overview",
+        "overview": `Pen test movie 1 video 1 overview ${port}`,
         "streamIds": ["pt1v1s1"]
     },
     "pt1v2": {
         "id": "pt1v2",
-        "title": "Pen test movie 1 video 2",
+        "title": `Pen test movie 1 video 2 ${port}`,
         "thumbnail": `http://127.0.0.1:${port}/assets/non-existing-thumbnail.jpg`,
         "released": "3333-11-08T07:57:48.271Z",
-        "overview": "Pen test movie 1 video 2 (broken thumbnail)",
+        "overview": `Pen test movie 1 video 2 (broken thumbnail) ${port}`,
         "streamIds": ["pt1v2s1"]
     },
     "pt1v3": {
         "id": "pt1v3",
-        "title": "Pen test movie 1 video 3",
+        "title": `Pen test movie 1 video 3 ${port}`,
         "released": "2019-11-08T07:57:48.271Z",
-        "overview": "Pen test movie 1 video 3 (no thumbnail)",
+        "overview": `Pen test movie 1 video 3 (no thumbnail) ${port}`,
         "streamIds": ["pt1v3s1"]
     }
 });
@@ -77,25 +77,25 @@ const pullAllVideos = (port) => ({
 const pullAllStreams = (port) => ({
     "pt1v1s1": {
         "id": "pt1v1s1",
-        "title": "Pen test movie 1 video 1 stream 1",
+        "title": `Pen test movie 1 video 1 stream 1 ${port}`,
         "thumbnail": `http://127.0.0.1:${port}/assets/poster.jpg`,
         "url": `http://127.0.0.1:${port}/assets/video.mp4`
     },
     "pt1v2s1": {
         "id": "pt1v2s1",
-        "title": "Pen test movie 1 video 2 stream 1",
+        "title": `Pen test movie 1 video 2 stream 1 ${port}`,
         "thumbnail": `http://127.0.0.1:${port}/assets/poster.jpg`,
         "url": `http://127.0.0.1:${port}/assets/video.mp4`
     },
     "pt1v3s1": {
         "id": "pt1v3s1",
-        "title": "Pen test movie 1 video 3 stream 1",
+        "title": `Pen test movie 1 video 3 stream 1 ${port}`,
         "thumbnail": `http://127.0.0.1:${port}/assets/poster.jpg`,
         "url": `http://127.0.0.1:${port}/assets/video.mp4`
     },
     "pt1t1": {
         "id": "pt1t1",
-        "title": "Pen test movie 1 trailer 1",
+        "title": `Pen test movie 1 trailer 1 ${port}`,
         "thumbnail": `http://127.0.0.1:${port}/assets/poster.jpg`,
         "url": `http://127.0.0.1:${port}/assets/video.mp4`
     }
